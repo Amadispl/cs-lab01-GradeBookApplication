582974949fe8eddc55d7b9b9e69d604db62b0341
Amadispl <pieterwas81@gmail.com>
2022-03-28 19:59:27 +0200

    13 test done

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    internal class StandardGradeBook:BaseGradeBook
+    public class StandardGradeBook:BaseGradeBook
     {
        public StandardGradeBook(String name):base(name)
         {
-            Name = name;
+            
             Type = Enums.GradeBookType.Standard;
         }
     }
