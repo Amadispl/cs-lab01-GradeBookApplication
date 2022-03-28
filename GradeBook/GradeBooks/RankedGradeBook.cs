1ddd5829ce101cfa31cf78e8bed866ad71e736df
Amadispl <pieterwas81@gmail.com>
2022-03-28 19:46:02 +0200

    5 test done

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,38 @@ namespace GradeBook.GradeBooks
             Name = name;
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count<5)
+            {
+                throw new InvalidOperationException();
+            }
+            int x = (int)Math.Ceiling(Students.Count * 0.20);
+            int l=0;
+            
+            for (int i = 0; i < Students.Count; i++)
+            {
+                double m=0;
+                for (int j = 0; j < Students[i].Grades.Count; j++)
+                {
+                    m=+ Students[i].Grades[j];   
+                }
+                if (averageGrade >= m / Students[i].Grades.Count)
+                {
+                    l++;
+                }
+            }
+            if (l< (int)Math.Ceiling(Students.Count * 0.20))
+                return 'F';
+            else if (l< (int)Math.Ceiling(Students.Count * 0.40))
+                return 'D';
+            else if (l < (int)Math.Ceiling(Students.Count * 0.60))
+                return 'C';
+            else if (l < (int)Math.Ceiling(Students.Count * 0.80))
+                return 'B';
+            else
+                return 'A';
+        }
     }
+    
 }
