7ee370501bc0d3b7b1372b20c47cb7812a66fc0b
Amadispl <pieterwas81@gmail.com>
2022-03-28 18:38:50 +0200

    3 test done

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    internal class StandardGradeBook:BaseGradeBook
+    {
+       public StandardGradeBook(String name):base(name)
+        {
+            Name = name;
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
