722322c632fe96de582016ef96ef3971597d32c3
Amadispl <pieterwas81@gmail.com>
2022-03-28 18:44:24 +0200

    4 test done

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook:BaseGradeBook
+    {
+        public RankedGradeBook(String name):base(name)
+        {
+            Name = name;
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
